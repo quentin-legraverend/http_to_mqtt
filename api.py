9ec28e8780233de6450eb67e5729739cd2704179
Quentin Le Graverend <quentin@legraverend.fr>
2020-07-31 11:56:06 +0200

    add token support on the API

--- api.py
+++ api.py
@@ -7,6 +7,7 @@ MQTT_HOST = os.getenv('MQTT_HOST', '127.0.0.1')
 MQTT_PORT = os.getenv('MQTT_PORT', 1883)
 BIND_HOST= os.getenv('BIND_HOST', '127.0.0.1')
 BIND_PORT = os.getenv('BIND_PORT', 5000)
+API_TOKEN = os.getenv('API_TOKEN', 'MY_TOKEN')
 
 app = Flask(__name__)
 
@@ -16,9 +17,14 @@ def get_id():
 
     topic = request.args.get('topic')
     message = request.args.get('message')
-    mqtt = mq.Client("http-to-mqtt")
-    mqtt.connect(MQTT_HOST, MQTT_PORT, 15)
-    mqtt.publish(topic, message)
+    token = request.args.get('token')
+    if token == API_TOKEN:
+        mqtt = mq.Client("http-to-mqtt")
+        mqtt.connect(MQTT_HOST, MQTT_PORT, 15)
+        mqtt.publish(topic, message)
+        return 'MQTT command published'
+    else:
+        return 'Wrong API_TOKEN specified'
 
 if __name__ == '__main__':
     print("Starting HTTP-to-MQTT server. MQTT_HOST is: {}:{}".format(MQTT_HOST, MQTT_PORT))
